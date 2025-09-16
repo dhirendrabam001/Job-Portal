2fff0d5bec28562bd5d6e981b4fe3593ff4ba484
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-09-16 03:21:13 -0700

    updated css

--- Job-Portal/src/DataStore/Data.jsx
+++ Job-Portal/src/DataStore/Data.jsx
@@ -9,3 +9,58 @@ export const companyInfo = [
   "Slack",
   "Spotify",
 ];
+
+export const CategoryList = [
+  {
+    img: "iocns-1.webp",
+    title: "Web Developer",
+    description:
+      "Build and maintain websites for clients with improvments business",
+    jobs: "2k+ new jobs posted",
+  },
+  {
+    img: "iocns-2.webp",
+    title: "Finance",
+    description: "Manage financial records and transactions for businessman",
+    jobs: "1k+ new jobs posted",
+  },
+  {
+    img: "iocns-3.webp",
+    title: "Arts & Design",
+    description:
+      "Create visual content for branding and media also social media platforms",
+    jobs: "800+ new jobs posted",
+  },
+  {
+    img: "iocns-4.webp",
+    title: "Content Writting",
+    description:
+      "Write and edit content for various platforms and content writing website",
+    jobs: "700+ new jobs posted",
+  },
+  {
+    img: "iocns-5.webp",
+    title: "Data Entry",
+    description: "Input data into systems accurately and efficiently",
+    jobs: "900+ new jobs posted",
+  },
+  {
+    img: "iocns-6.webp",
+    title: "Digital Marketing",
+    description: "Promote brands online with marketing strategies",
+    jobs: "650+ new jobs posted",
+  },
+  {
+    img: "iocns-7.webp",
+    title: "Human Resources",
+    description: "Recruit, manage, and support company employees",
+    jobs: "500+ new jobs posted",
+  },
+
+  {
+    img: "iocns-8.webp",
+    title: "UI-UX Designer",
+    description: "Design user interfaces and enhance user experience",
+    jobs: "500+ new jobs posted",
+  },
+];
--- Job-Portal/src/LandingPage/JobCategory.jsx
+++ Job-Portal/src/LandingPage/JobCategory.jsx
@@ -1,7 +1,67 @@
+import { CategoryList } from "../DataStore/Data";
+import Slider from "react-slick";
+
 const JobCategory = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // number of cards visible
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 1024, // tablets
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 600, // mobile
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
+
   return (
-    <div className="job-category">
-      <div className="container"></div>
+    <div className="job-category py-4 mt-4">
+      <div className="container">
+        <div className="text-center mb-2">
+          <h2 className="fw-bold fs-1 text-white">
+            Browse <span>Jobs</span> Category
+          </h2>
+          <p>
+            Explore a wide range of job categories from IT and engineering to
+            marketing, finance, healthcare, and more.
+            <br />
+            Find the right role that matches your skills, interests, and goals,
+            and take the next step toward building your future.
+          </p>
+        </div>
+
+        {/* Carousel */}
+        <Slider {...settings}>
+          {CategoryList.map((items, key) => (
+            <div key={key} className="">
+              {" "}
+              {/* padding for spacing */}
+              <div className="card mx-3 text-center shadow">
+                <img
+                  src={`${items.img}`}
+                  className="card-img-top"
+                  alt={items.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{items.title}</h5>
+                  <p className="card-text">{items.description}</p>
+                  <p className="text-info">
+                    <small className="text-muted">{items.jobs}</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
